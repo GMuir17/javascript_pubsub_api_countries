6dc2fc2304b76033b519c319f4a19904d94cafe9
Gary Muir <garymuir17@gmail.com>
2018-08-08 13:53:00 +0100

    adds flags

--- src/views/country_info_view.js
+++ src/views/country_info_view.js
@@ -16,12 +16,22 @@ CountryInfoView.prototype.render = function (country) {
   this.container.innerHTML = '';
   const countryName = country.name;
   this.renderName(countryName);
+
+  const countryFlag = country.flag;
+  this.renderFlag(countryFlag);
 };
 
 CountryInfoView.prototype.renderName = function (countryName) {
   const countryNameElement = document.createElement("h2");
   countryNameElement.textContent = countryName;
   this.container.appendChild(countryNameElement);
+};
+
+CountryInfoView.prototype.renderFlag = function (countryFlag) {
+  const countryFlagElement = document.createElement("img");
+  // countryFlagElement.classList.add("medium-image");
+  countryFlagElement.src = countryFlag;
+  this.container.appendChild(countryFlagElement);
 };
 
 module.exports = CountryInfoView;
